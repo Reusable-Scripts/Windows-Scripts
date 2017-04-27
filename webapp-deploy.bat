7fc502f27ca981627503135342f40683ef2edb66
R Dama <damarak630@gmail.com>
2017-04-26 17:12:49 -0700

    Update webapp-deploy.bat

--- webapp-deploy.bat
+++ webapp-deploy.bat
@@ -4,7 +4,7 @@ echo %APP_VERSION%
 set APP_VERSION=%APP_VERSION%
 REM This script only deploy's web application ans restarts tomcat
 SET Webapps_Dir="%CATALLINA_HOME%"\webapps
-call xcopy \\klasj\ktfiles\Divisions\EBEAM\Releases\SCM\Smarts\"%APP_VERSIOn%"\sots\*.war "%Webapps_Dir%" /Y /I /R
+call xcopy \\\Divisions\***\****\SCM\***\"%APP_VERSIOn%"\appname\*.war "%Webapps_Dir%" /Y /I /R
 call "%CATALINA_HOME%"\bin\catalina.bat stop
 timeout /t 30
 call '%CATALINA_HOME%'\bin\catalina.bat start
