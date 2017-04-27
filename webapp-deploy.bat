7969c5bc93f1192dd21ca31737c8467f07129619
R Dama <damarak630@gmail.com>
2017-04-26 17:32:37 -0700

    Update webapp-deploy.bat

--- webapp-deploy.bat
+++ webapp-deploy.bat
@@ -1,14 +1,22 @@
 @ECHO OFF
-set /p APP_VERSION="Enter Application Version without any spaces: "
-set /p MOD_NAME="Enter Module Name without spaces: "
+set /p APP_VERSION="Enter Application Version: "
+set /p APP_NAME="Enter Application Name: "
 echo %APP_VERSION%
-echo %MOD_NAME%
+echo %APP_NAME%
+if "%APP_NAME%" == "app1" goto Deploy
+if "%APP_NAME%" == "app2" goto Deploy
+goto end
+:Deploy
 set APP_VERSION=%APP_VERSION%
-set MOD_NAME=%MOD_NAME%
+set APP_NAME=%APP_NAME%
 REM This script only deploy's web application ans restarts tomcat
 SET Webapps_Dir="%CATALLINA_HOME%"\webapps
-call xcopy \\\Divisions\***\****\SCM\***\"%APP_VERSIOn%"\"%MOD_NAME%"\*.war "%Webapps_Dir%" /Y /I /R
+call xcopy \\Share_Drive_Location\SCM\project\"%APP_VERSIOn%"\"%APP_NAME%"\*.war "%Webapps_Dir%" /Y /I /R
 call "%CATALINA_HOME%"\bin\catalina.bat stop
 timeout /t 30
 call "%CATALINA_HOME%"\bin\catalina.bat start
 endlocal.
+ :end
+ echo end
+ exit
+ 
