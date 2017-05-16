67bdfeba21ba67066acd4409c14670bcff8f5fed
R Dama <damarak630@gmail.com>
2017-05-16 14:02:57 -0700

    Update Install-Tomcat.bat

--- Install-Tomcat.bat
+++ Install-Tomcat.bat
@@ -11,6 +11,19 @@ setx CATALINA_HOME="%InstallDir%\apache-tomcat-9.0.0.M20"
 setx path "%path%;%CATALINA_HOME%\bin"
 echo "end of installation"
 
+cd /d %CATALINA_HOME%\conf
+setLocal EnableDelayedExpansion
+For /f "tokens=* delims= " %%a in (server.xml) do (
+Set str=%%a
+set str=!str:8080=8083!
+echo !str!>>New.xml
+)
+ENDLOCAL
+move New.xml server.xml
+
+cd /d %CATALINA_HOME%
+bin\startup.bat
+
 goto comments
 1. To update tomcat connector port
 Here's how you can start in Linux (assuming your current directory is CATALINA_HOME):
