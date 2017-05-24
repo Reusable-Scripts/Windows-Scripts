3c18694d954ecf621593a8eb96a2a64d929b01bb
R Dama <damarak630@gmail.com>
2017-05-24 16:43:32 -0700

    Update 7zipextract.bat

--- 7zipextract.bat
+++ 7zipextract.bat
@@ -1,1 +1,4 @@
 7zip.exe x sourc.zip -oextractfoldername -r -y
+
+download 7zip software zip file and extract. copy 7za.exe to required location.
+call .\Packages\7za.exe x .\Packages\apache-tomcat-8.5.15-windows-x64.zip -o.\Packages\apache-test * -r -y
