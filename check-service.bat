f6d70951a7bc24f71f4e4b7325ba5822aaac1605
R Dama <damarak630@gmail.com>
2017-10-19 14:57:20 -0700

    Create check-service.bat

---
+++ check-service.bat
@@ -0,0 +1,16 @@
+@echo off
+set "ServiceName=%APP-NAME%-%Version%-%HTTP-PORT%"
+SC queryex "%ServiceName%"|Find "STATE"|Find /v "RUNNING">Nul&&(
+    echo %ServiceName% not running 
+    echo Start %ServiceName%
+
+    Net start "%ServiceName%">nul||(
+        Echo "%ServiceName%" wont start 
+        exit /b 1
+    )
+    echo "%ServiceName%" started
+    exit /b 0
+)||(
+    echo "%ServiceName%" working
+    exit /b 0
+)
